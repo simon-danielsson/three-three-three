3a741c1627080806d8612488fca9f04c3848deb2
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-09-10 13:48:53 +0200

    fix(main.c): undo prev. refactor since it broke the logic

--- src/main.c
+++ src/main.c
@@ -175,8 +175,9 @@ WinState check_win_state(BoardCell b[BOARD_SZ][BOARD_SZ]) {
     int bot_matches;
     int board_full = 1;
 
-    for (player_matches = 0, bot_matches = 0, win_cond = 0; win_cond < 8;
-            win_cond++) {
+    for (win_cond = 0; win_cond < 8; win_cond++) {
+        player_matches = 0;
+        bot_matches = 0;
 
         for (i = 0; i < BOARD_SZ; i++) {
             for (j = 0; j < BOARD_SZ; j++) {
